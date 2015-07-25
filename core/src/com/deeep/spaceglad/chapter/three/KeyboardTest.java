7119c15a3ea8aa5fd30d663a7c21da32db7dea8a
Kruhlmann <andreaskruhlmann@gmail.com>
2015-07-26 00:01:23 +0200

    Keyboard test finished :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope: :noscope:

--- core/src/com/deeep/spaceglad/chapter/three/KeyboardTest.java
+++ core/src/com/deeep/spaceglad/chapter/three/KeyboardTest.java
@@ -1,20 +1,35 @@
 package com.deeep.spaceglad.chapter.three;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 
 public class KeyboardTest extends ApplicationAdapter{
 
-    public TextListener listener;
+    public String inputString = "";
 
     @Override
     public void create () {
         System.out.println("|Input test|");
-        listener = new TextListener();
     }
 
     @Override
     public void render () {
-        listener.update();
+        if(Gdx.input.isKeyPressed(Input.Keys.ENTER)) outputString();
+        if(Gdx.input.isKeyPressed(Input.Keys.BACKSPACE) && inputString.length() > 0) inputString = inputString.substring(1, inputString.length());
+
+        if(Gdx.input.isKeyPressed(Input.Keys.A)) inputString += "A";
+        if(Gdx.input.isKeyPressed(Input.Keys.B)) inputString += "B";
+        if(Gdx.input.isKeyPressed(Input.Keys.C)) inputString += "C";
+        // ...
+        if(Gdx.input.isKeyPressed(Input.Keys.X)) inputString += "X";
+        if(Gdx.input.isKeyPressed(Input.Keys.Y)) inputString += "Y";
+        if(Gdx.input.isKeyPressed(Input.Keys.Z)) inputString += "Z";
     }
 
+    public void outputString() {
+        if(inputString.equals("")) return;
+        System.out.println(inputString);
+        inputString = "";
+    }
 }
