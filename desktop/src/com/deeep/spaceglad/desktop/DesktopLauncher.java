55befd9aab12b2de2e50ea941fe76c715d08923f
Elmar <elmarblabla@hotmail.com>
2015-07-25 17:57:43 +0200

    CameraTest

--- desktop/src/com/deeep/spaceglad/desktop/DesktopLauncher.java
+++ desktop/src/com/deeep/spaceglad/desktop/DesktopLauncher.java
@@ -3,10 +3,11 @@ package com.deeep.spaceglad.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.deeep.spaceglad.Core;
+import com.deeep.spaceglad.chapter.two.CameraTest;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Core(), config);
+		new LwjglApplication(new CameraTest(), config);
 	}
 }
