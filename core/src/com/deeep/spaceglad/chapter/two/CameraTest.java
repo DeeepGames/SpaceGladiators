e9acd5864dbe65a122323d424977592db529a1c0
Elmar <elmarblabla@hotmail.com>
2015-07-25 19:05:07 +0200

    3D test

--- core/src/com/deeep/spaceglad/chapter/two/CameraTest.java
+++ core/src/com/deeep/spaceglad/chapter/two/CameraTest.java
@@ -2,29 +2,70 @@ package com.deeep.spaceglad.chapter.two;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 /**
  * Created by Elmar on 25-7-2015.
  */
 public class CameraTest extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
-
+   /* SpriteBatch batch;
+    Texture img;*/
+    public PerspectiveCamera cam;
+    public Model model;
+    public Model model2;
+    public ModelInstance instance;
+    public ModelInstance instance2;
+    public ModelBatch modelBatch;
+    FirstPersonCameraController firstPersonCameraController;
+    public Environment environment;
     @Override
     public void create () {
-        batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+
+        /*batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");*/
+
+
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+        modelBatch = new ModelBatch();
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(20f, 2f, 20f);
+        cam.lookAt(0,0,0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createBox(5f, 5f, 5f,
+                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+        model2 = modelBuilder.createBox(50f, 1f, 50f,
+                new Material(ColorAttribute.createDiffuse(Color.RED)),
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+        instance = new ModelInstance(model);
+        instance2 = new ModelInstance(model2);
+        firstPersonCameraController = new FirstPersonCameraController(cam);
+        Gdx.input.setInputProcessor(firstPersonCameraController);
     }
 
     @Override
     public void render () {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
+        firstPersonCameraController.update();
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        /*batch.begin();
         batch.draw(img, 0, 0);
-        batch.end();
+        batch.end();*/
+        modelBatch.begin(cam);
+        modelBatch.render(instance, environment);
+        modelBatch.render(instance2, environment);
+        modelBatch.end();
     }
 }
