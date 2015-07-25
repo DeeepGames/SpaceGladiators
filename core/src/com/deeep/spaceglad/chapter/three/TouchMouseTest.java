5639c8701c5e2cd59142ea06bb38558db9e69c90
Sebastián <scanevaro@hotmail.com>
2015-07-25 19:47:02 -0300

    :noscope: touch mouse test :noscope:

--- core/src/com/deeep/spaceglad/chapter/three/TouchMouseTest.java
+++ core/src/com/deeep/spaceglad/chapter/three/TouchMouseTest.java
@@ -1,7 +1,15 @@
 package com.deeep.spaceglad.chapter.three;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+
 /**
  * Created by scanevaro on 25/07/2015.
  */
-public class TouchMouseTest {
+public class TouchMouseTest extends ApplicationAdapter {
+    @Override
+    public void render() {
+        if (Gdx.input.justTouched())
+            System.out.println("justTouched");
+    }
 }
