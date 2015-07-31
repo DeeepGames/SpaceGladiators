9e266834111a7168458620cb19d5de0d4958c792
Sebastián <scanevaro@hotmail.com>
2015-07-31 18:34:46 -0300

    :noscope: structure :noscope:

--- core/src/com/deeep/spaceglad/Core.java
+++ core/src/com/deeep/spaceglad/Core.java
@@ -2,28 +2,30 @@ package com.deeep.spaceglad;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.deeep.spaceglad.chapter.seven.SoundManager;
 
 public class Core extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		SoundManager.getInstance().enemyGrowl.play();
-	}
+    SpriteBatch batch;
+    Texture img;
+    Screen screen;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        SoundManager.getInstance().enemyGrowl.play();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
 }
