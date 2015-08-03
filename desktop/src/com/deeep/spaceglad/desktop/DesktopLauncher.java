6fe3b0d471a4160dba6a0833817ec4bd3df8c528
Sebastián <scanevaro@hotmail.com>
2015-08-03 16:01:30 -0300

    :noscope: resize :noscope:

--- desktop/src/com/deeep/spaceglad/desktop/DesktopLauncher.java
+++ desktop/src/com/deeep/spaceglad/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.deeep.spaceglad.Core;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = (int) Core.VIRTUAL_WIDTH;
+        config.height = (int) Core.VIRTUAL_HEIGHT;
         new LwjglApplication(new Core(), config);
     }
 }
