88938fabf97d0fdd1816574bb611d7722ce60ea1
Kruhlmann <andreaskruhlmann@gmail.com>
2015-10-24 14:33:00 +0200

    crosshair works

--- core/src/com/deeep/spaceglad/UI/CrosshairWidget.java
+++ core/src/com/deeep/spaceglad/UI/CrosshairWidget.java
@@ -15,46 +15,47 @@ import com.deeep.spaceglad.Settings;
 public class CrosshairWidget extends Actor {
 
     private Image crosshairDot, crosshairOuterRing, crosshairInnerRing;
-    private float rotationSpeed;
+    private float outerRotationSpeed, innerRotationSpeed;
 
     public CrosshairWidget() {
         crosshairDot = new Image(new Texture(Gdx.files.internal("crosshair/crossHairPoint.png")));
         crosshairInnerRing = new Image(new Texture(Gdx.files.internal("crosshair/crossHairInnerRing.png")));
         crosshairOuterRing = new Image(new Texture(Gdx.files.internal("crosshair/crossHairOuterRing.png")));
-        rotationSpeed = 1F;
+        outerRotationSpeed = 1F;
+        innerRotationSpeed = -1F;
     }
 
     @Override
     public void act(float delta) {
         if (Settings.Paused) return;
-        //crosshairInnerRing.rotateBy(rotationSpeed);
-        crosshairOuterRing.rotateBy(rotationSpeed);
+        crosshairInnerRing.rotateBy(innerRotationSpeed);
+        crosshairOuterRing.rotateBy(outerRotationSpeed);
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         if (Settings.Paused) return;
         crosshairDot.draw(batch, parentAlpha);
-        //crosshairInnerRing.draw(batch, parentAlpha);
-        //crosshairOuterRing.draw(batch, parentAlpha);
+        crosshairInnerRing.draw(batch, parentAlpha);
+        crosshairOuterRing.draw(batch, parentAlpha);
     }
 
     @Override
     public void setPosition(float x, float y) {
         super.setPosition(x, y);
-        crosshairDot.setPosition(x, y);
-        crosshairInnerRing.setPosition(x, y);
+        crosshairDot.setPosition(x - 16, y - 16);
+        crosshairInnerRing.setPosition(x - 16, y - 16);
         crosshairOuterRing.setPosition(x - 16, y - 16);
-        crosshairInnerRing.setOrigin(x, y);
-        crosshairOuterRing.setOrigin(x - 16, y - 16);
+        crosshairInnerRing.setOrigin(crosshairInnerRing.getWidth() / 2, crosshairInnerRing.getHeight() / 2);
+        crosshairOuterRing.setOrigin(crosshairOuterRing.getWidth() / 4, crosshairOuterRing.getHeight() / 4);
         Logger.log(Logger.ANDREAS, Logger.INFO, "Setting origin to " + x + ", " + y);
     }
 
     @Override
     public void setSize(float width, float height) {
         super.setSize(width, height);
-        crosshairDot.setSize(width, height);
-        crosshairInnerRing.setSize(width, height);
+        crosshairDot.setSize(width * 2, height * 2);
+        crosshairInnerRing.setSize(width * 2, height * 2);
         crosshairOuterRing.setSize(width * 2, height * 2);
     }
 
